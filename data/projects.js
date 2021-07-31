f18ef4bb5203cb40002a5e40b80eb3014b82bc1e
Rahul5430 <rahul213himani@gmail.com>
2021-08-01 02:03:57 +0530

    added some more projects

--- data/projects.js
+++ data/projects.js
@@ -79,92 +79,110 @@ const projects = [
 
 export const extraProjects = [
     {
-        name: 'Project 1',
-        type: 'Type 1',
-        description: 'Description 1',
-        tech: ['Tech-1', 'Tech-2'],
+        name: 'LiveCodeShare',
+        type: 'Personal',
+        description: "A Node.JS based live code sharing application. You can chat and video call also while sharing code.",
+        tech: ['Express', 'Node.js', 'MongoDB', 'Socket.IO'],
         links: [
-            {
-                label: 'Website',
-                icon: 'link',
-                url: '',
-            },
+            // {
+            //     label: 'Website',
+            //     icon: 'link',
+            //     url: '',
+            // },
             {
                 label: 'Github',
                 icon: 'github',
-                url: '',
+                url: 'https://github.com/Rahul5430/LiveCodeShare',
             },
         ],
     },
     {
-        name: 'Project 2',
-        type: 'Type 2',
-        description: 'Description 2',
-        tech: ['Tech-1', 'Tech-2'],
+        name: 'React Meals',
+        type: 'Personal',
+        description: 'React Meals is frontend food ordering app built with React.',
+        tech: ['React'],
         links: [
             {
                 label: 'Website',
                 icon: 'link',
-                url: '',
+                url: 'https://rahul5430.github.io/react-meals/',
             },
             {
                 label: 'Github',
                 icon: 'github',
-                url: '',
+                url: 'https://github.com/Rahul5430/react-meals',
             },
         ],
     },
     {
-        name: 'Project 3',
-        type: 'Type 3',
-        description: 'Description 3',
-        tech: ['Tech-1', 'Tech-2'],
+        name: 'Blog',
+        type: 'Personal',
+        description: "A blogging website built with Express and Node.js. You can blog as much as you want and even view other's blogs.",
+        tech: ['Express', 'Node.js'],
         links: [
-            {
-                label: 'Website',
-                icon: 'link',
-                url: '',
-            },
+            // {
+            //     label: 'Website',
+            //     icon: 'link',
+            //     url: '',
+            // },
             {
                 label: 'Github',
                 icon: 'github',
-                url: '',
+                url: 'https://github.com/Rahul5430/Blog',
             },
         ],
     },
     {
-        name: 'Project 4',
-        type: 'Type 4',
-        description: 'Description 4',
-        tech: ['Tech-1', 'Tech-2'],
+        name: 'Tic-Tac-Toe',
+        type: 'Personal',
+        description: "Presenting to you everyone's favourite game - Tic-Tac-Toe. You can play with your friend or you can also play with the computer.",
+        tech: ['JavaScript'],
         links: [
             {
                 label: 'Website',
                 icon: 'link',
-                url: '',
+                url: 'https://rahul5430.github.io/Tic-Tac-Toe/',
             },
             {
                 label: 'Github',
                 icon: 'github',
-                url: '',
+                url: 'https://github.com/Rahul5430/Tic-Tac-Toe',
             },
         ],
     },
     {
-        name: 'Project 5',
-        type: 'Type 5',
-        description: 'Description 5',
-        tech: ['Tech-1', 'Tech-2'],
+        name: 'Caterpillar',
+        type: 'Personal',
+        description: "Your favourite Caterpillar game also known as 'Snake' game built with python. Play as much as you want but keep in mind that caterpillar's speed increases after every fruit he eats.",
+        tech: ['Python'],
         links: [
-            {
-                label: 'Website',
-                icon: 'link',
-                url: '',
-            },
+            // {
+            //     label: 'Website',
+            //     icon: 'link',
+            //     url: '',
+            // },
             {
                 label: 'Github',
                 icon: 'github',
-                url: '',
+                url: 'https://github.com/Rahul5430/Python-Projects/tree/master/Caterpillar',
+            },
+        ],
+    },
+    {
+        name: 'Matchmaker',
+        type: 'Personal',
+        description: "Let's test your memory in this matchmaking card game built with python. Just try not to cheat.",
+        tech: ['Python'],
+        links: [
+            // {
+            //     label: 'Website',
+            //     icon: 'link',
+            //     url: '',
+            // },
+            {
+                label: 'Github',
+                icon: 'github',
+                url: 'https://github.com/Rahul5430/Python-Projects/tree/master/Matchmaker',
             },
         ],
     },
