bbfc61548851f5b6ecfbfbc7b5158bc9f62e4791
Rahul5430 <rahul213himani@gmail.com>
2021-08-03 00:08:04 +0530

    added author

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ const SiteHead = ({ title }) => (
     <title>{title}</title>
     <meta name="title" content="Rahul Sharma - Full Stack Developer" />
     <meta name="description" content="Rahul Sharma - Portfolio" />
+    <meta name="author" content="Rahul Sharma" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
