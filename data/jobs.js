7e2161af60effffc73cecaa8c497eb648ad379d4
Rahul5430 <rahul213himani@gmail.com>
2021-12-06 23:27:23 +0530

    added padhakku

--- data/jobs.js
+++ data/jobs.js
@@ -1,8 +1,18 @@
 const jobs = [
     {
+        workplace: 'Padhakku',
+        position: 'MERN Stack Web Developer',
+        duration: ['Sep 2021 - Nov 2021',],
+        description: [
+            'Developed Padhakku Peek A Book website.',
+            'Built the frontend using React and implemented UI/UX using Chakra-UI and Material UI.',
+        ],
+        url: '#',
+    },
+    {
         workplace: 'ISB',
         position: 'Full Stack Web Developer',
-        duration: ['Apr 2021 - Present', 'Dec 2020 - Feb 2021'],
+        duration: ['Apr 2021 - Aug 2021', 'Dec 2020 - Feb 2021'],
         description: [
             'Developed the Option Strategy Builder using AngularJS and Flask.',
             'Devised various strategies which processed, analyzed and rendered the data visually on a chart.',
